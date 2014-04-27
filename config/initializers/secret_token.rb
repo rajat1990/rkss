3e20ca98765f40f61e4dc8094ebc5c7969bc56bc
rajat <rks.exe@gmail.com>
2014-04-27 13:11:26 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rkss::Application.config.secret_key_base = '55bba227ac359aa685910db9ba43f737191c0f8e32b65bd5e87f0935c42ab3831e5a46d161c50ef7c407351d8e4cdd1fdcdc177bb6e5f24159c44d43d13b781b'
